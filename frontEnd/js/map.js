e571881d062aa9cdc12295854157dd5ddc3ab07b
Cristian Franco <cristianfranco@Cristians-MacBook-Pro.local>
2018-08-29 00:01:09 -0500

    Arreglos en las vistas y agregamos whatsapp a todas las vitas

--- frontEnd/js/map.js
+++ frontEnd/js/map.js
@@ -3,7 +3,7 @@
 		   //Google Map
 
 			var mapCanvas = document.getElementById('map-canvas');
-			var myLatlng = new google.maps.LatLng(-22.969778, -43.186859); //your latitude and longitude here
+			var myLatlng = new google.maps.LatLng(6.134460, -75.608177); //your latitude and longitude here
 			var mapOptions = {
 				zoom: 17,
 				scrollwheel: false,
@@ -19,19 +19,4 @@
 			});
 
 
-			var mapCanvas2 = document.getElementById('map-canvas2');
-			var myLatlng2 = new google.maps.LatLng(-22.969778, -43.186859); //your latitude and longitude here
-			var mapOptions2 = {
-				zoom: 17,
-				scrollwheel: false,
-				center: myLatlng,
-				styles:[ { "featureType": "administrative.country", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "administrative.country", "elementType": "labels.text.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "poi", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "visibility": "on" } ] } ]
-			}
-			var map2 = new google.maps.Map(mapCanvas2, mapOptions2)
-			var marker = new google.maps.Marker({
-				position: myLatlng,
-				icon: 'img/mapmarker.png',
-				map: map2,
-				title: ''
-			});
 		});
